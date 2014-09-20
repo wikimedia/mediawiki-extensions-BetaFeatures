0eafedbb1642552e396e10d47e46826c45dfb2ee
Bartosz Dziewoński <matma.rex@gmail.com>
2014-09-20 20:36:17 +0200

    Use the existing .client-js class to hide items when JS is enabled
    Change-Id: I63caf8f3c78bb5df3a4492029df6bf0023150177

--- resources/betafeatures.js
+++ resources/betafeatures.js
@@ -30,9 +30,6 @@
 			$field = $input.closest( '.mw-ui-feature-field' );
 			blacklist = features[preference] && features[preference].blacklist;
 
-			// We're here so JavaScript works
-			$field.find( '.mw-ui-feature-requirements-javascript' ).hide();
-
 			if ( blacklist && $.client.test( blacklist, null, true ) ) {
 				// Browser not compatible
 				$field.find( '.mw-ui-feature-requirements-browser' ).show();
