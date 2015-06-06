c3416813fdce6463bc5c512187e9b8b1a2c9cbce
James D. Forrester <jforrester@wikimedia.org>
2015-06-05 17:14:54 -0700

    build: Add some development dependencies
    Adding jscs and jshint for consistency.
    
    Change-Id: Idd285ffb50b8c4b513633a48c2db48f11bcb4ef7

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,18 +8,51 @@
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+	var conf = grunt.file.readJSON( 'extension.json' );
+
 	grunt.initConfig( {
-		banana: {
-			all: ['i18n/']
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'*.js',
+				'resources/*.js'
+			]
+		},
+		jscs: {
+			fix: {
+				options: {
+					fix: true
+				},
+				src: '<%= jshint.all %>'
+			},
+			main: {
+				src: '<%= jshint.all %>'
+			}
 		},
 		jsonlint: {
 			all: [
 				'**/*.json',
 				'!node_modules/**'
 			]
+		},
+		banana: conf.MessagesDirs,
+		watch: {
+			files: [
+				'.{jscsrc,jshintignore,jshintrc}',
+				'<%= jshint.all %>',
+				'<%= jsonlint.all %>'
+			],
+			tasks: 'test'
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jscs:main', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'fix', 'jscs:fix' );
 	grunt.registerTask( 'default', 'test' );
 };
