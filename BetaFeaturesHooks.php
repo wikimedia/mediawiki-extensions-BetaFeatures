43a74ebf7d1aa7f677df8d25e7e3a7646052609e
Krinkle <krinklemail@gmail.com>
2016-03-07 21:32:28 +0000

    Revert "Add version hash to screenshot urls"
    PHP Warning: strpos() expects parameter 1 to be string, array given.
    
    It contains an array keyed by language code or language direction.
    The hash must apply to its values, not the array itself.
    
    Should probably be moved to HTMLFeatureField.
    
    This reverts commit 57f974cb50565571b5aba12b1c1496d87042a97c.
    
    Change-Id: Ia39dc54099b2a0ab3a670402b85f534142f3f466

--- BetaFeaturesHooks.php
+++ BetaFeaturesHooks.php
@@ -104,7 +104,6 @@ class BetaFeaturesHooks {
 	public static function getPreferences( User $user, array &$prefs ) {
 		global $wgBetaFeaturesWhitelist, $wgBetaFeatures;
 
-		$config = ConfigFactory::getDefaultInstance()->makeConfig( 'main' );
 		$betaPrefs = $wgBetaFeatures;
 		$depHooks = array();
 
@@ -177,7 +176,7 @@ class BetaFeaturesHooks {
 				'section' => 'betafeatures',
 			);
 
-			$fields = array(
+			$requiredFields = array(
 				'label-message' => true,
 				'desc-message' => true,
 				'screenshot' => false,
@@ -188,7 +187,7 @@ class BetaFeaturesHooks {
 				'discussion-message' => false,
 			);
 
-			foreach ( $fields as $field => $required ) {
+			foreach ( $requiredFields as $field => $required ) {
 				if ( isset( $info[$field] ) ) {
 					$opt[$field] = $info[$field];
 				} elseif ( $required ) {
@@ -199,13 +198,6 @@ class BetaFeaturesHooks {
 						"The field {$field} was missing from the beta feature {$key}."
 					);
 				}
-			}
-
-			if ( isset( $opt['screenshot'] ) ) {
-				// Ensure image urls have a version hash.
-				$opt['screenshot'] = OutputPage::transformResourcePath( $config,
-					$opt['screenshot']
-				);
 			}
 
 			if ( isset( $counts[$key] ) ) {
