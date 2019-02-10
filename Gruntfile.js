7ab632a07980934477f0fa4b11e9ccb14db6944e
Ed Sanders <esanders@wikimedia.org>
2019-02-10 15:14:43 +0000

    build: Update devDependencies
    Change-Id: Ieb3a0f1962d487a72e1a4ea78c72ca9b2706aa88

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,9 @@ module.exports = function ( grunt ) {
 
 	grunt.initConfig( {
 		eslint: {
+			options: {
+				reportUnusedDisableDirectives: true
+			},
 			all: [
 				'**/*.js',
 				'!node_modules/**',
