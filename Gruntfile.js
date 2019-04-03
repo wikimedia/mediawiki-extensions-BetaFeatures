7d871302d1e378f74170ebf8d88d4960d0780823
James D. Forrester <jforrester@wikimedia.org>
2019-04-03 15:47:56 -0700

    build: Upgrade eslint-config-wikimedia from 0.10.1 to 0.11.0
    Change-Id: Ibd25ede2b9759ea6ad9b5f7fc1dff955100f79ed

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,8 @@ module.exports = function ( grunt ) {
 	grunt.initConfig( {
 		eslint: {
 			options: {
-				reportUnusedDisableDirectives: true
+				reportUnusedDisableDirectives: true,
+				cache: true
 			},
 			all: [
 				'**/*.js',
