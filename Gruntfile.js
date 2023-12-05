b272de35207fc2a82ab1642c1383aa5e0e81d8db
Ed Sanders <esanders@wikimedia.org>
2023-12-05 15:33:25 +0000

    build: Update linters
    Change-Id: I5e8ec391a6cbbacd6595707e9b49f2fe9c8ce966

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,10 +19,7 @@ module.exports = function ( grunt ) {
 				cache: true,
 				fix: grunt.option( 'fix' )
 			},
-			all: [
-				'**/*.{js,json}',
-				'!{node_modules,vendor}/**'
-			]
+			all: [ '.' ]
 		},
 		stylelint: {
 			all: [
